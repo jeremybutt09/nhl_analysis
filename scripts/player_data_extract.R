e2c4a44e30bce9fcf8dd1406ca6c1e6632ef8590
Jeremy Butt <jbutt09@gmail.com>
2022-09-21 22:58:25 -0600

    making function more robust and can handle missing columns in API call. This was an issue as some API calls did not have faceOffPct column. Explict arrange of columns to ensure that each time function is called, columns are in correct order.

--- scripts/player_data_extract.R
+++ scripts/player_data_extract.R
@@ -11,6 +11,33 @@ goalie_output_file <- "data/goalie_game_data.csv"
 #FILE WITH SCHEDULE DATA. 
 schedule_file <- "data/schedule_data.csv"
 
+#CREATING A VECTOR OF COLUMN NAMES WITH THE CORRESPONDING NA VALUES. THIS WILL BE USED IN THE CASE
+#A COLUMN DOESN'T EXIST IN API CALL. NOT SURE THE REASON BUT IT SEEMS SOMETIMES THAT faceOffPct COLUMN
+#IS NOT AVAILABLE IN API CALL
+col <- c(PLAYER_ID = NA_integer_,
+         PLAYER_NAME = NA_character_,
+         PRIMARY_POSITION_CODE = NA_character_,
+         TIME_ON_ICE = NA_character_,
+         ASSISTS = NA_integer_,
+         GOALS = NA_integer_,
+         SHOTS = NA_integer_,
+         HITS = NA_integer_,
+         POWER_PLAY_GOALS = NA_integer_,
+         POWER_PLAY_ASSISTS = NA_integer_,
+         PENALTY_MINUTES = NA_integer_,
+         FACE_OFF_WINS = NA_integer_,
+         FACEOFF_TAKEN = NA_integer_,
+         TAKEAWAYS = NA_integer_,
+         GIVEAWAYS = NA_integer_,
+         SHORT_HANDED_GOALS = NA_integer_,
+         SHORT_HANDED_ASSISTS = NA_integer_,
+         BLOCKED = NA_integer_,
+         PLUS_MINUS = NA_integer_,
+         EVEN_TIME_ON_ICE = NA_character_,
+         POWER_PLAY_TIME_ON_ICE = NA_character_,
+         SHORT_HANDED_TIME_ON_ICE = NA_character_,
+         FACE_OFF_PCT = NA_real_)
+
 #LOADING ONLY GAME_PK DATA. THIS WILL BE RUN THROUGH A LOOP
 game_id <- read_csv(file = schedule_file) %>%
     filter(DATE >= as_date("2016-09-01"),
@@ -31,26 +58,50 @@ player_game_data_extract <- function(game_id, home_away) {
         unnest_wider(player_id) %>%
         unnest_wider(stats) %>%
         unnest_wider(skaterStats,
-                     names_sep = "_") %>%
+                     names_sep = "_") %>% #SUFFIXING COLUMN NAMES WITH UNNESTED LIST COLUMN SEP BY _
         hoist(person,
               player_id = list("id"),
               player_name = list("fullName"),
               primary_position_code = list("primaryPosition", "code")) %>%
-        filter(primary_position_code != "G",
-               !is.na(skaterStats_timeOnIce)) %>%
+        filter(primary_position_code != "G",      #REMOVING GOALIES. THIS FUNCTION IS FOR PLAYER
+               !is.na(skaterStats_timeOnIce)) %>% #DON'T WANT TO INCLUDE PLAYER WHO DIDN'T PLAY
         select(player_id,
                player_name,
                primary_position_code,
-               starts_with("skaterStats_")) %>%
+               starts_with("skaterStats_")) %>% #COLUMNS WITHIN skaterStats LIST COLUMN VARY FROM API CALL. THIS ENSURE ALL skaterStat COLUMNS ARE RETURNED
         rename_with(.,
                     str_replace_all,
-                    pattern = "(?<=[a-z0-9])(?=[A-Z])",
+                    pattern = "(?<=[a-z0-9])(?=[A-Z])", #ALL CAMEL CASE WILL BE SEPERATED WITH _
                     replacement = "_") %>%
         rename_with(.,
-                    str_to_upper) %>%
+                    str_to_upper) %>% #ALL COLUMNS TO UPPER CASE
         rename_with(.,
                     str_remove,
-                    pattern = "SKATER_STATS_")
+                    pattern = "SKATER_STATS_") %>% #REMOVING THIS PATTERN TO MAKE COLUMN NAMES MORE SIMPLY.
+        add_column(!!!col[!names(col) %in% names(.)]) %>% #ADDING MISSING COLUMNS FROM THE 
+        select(PLAYER_ID,  #EXPLICITY CHOOSING ORDER OF COLUMNS TO ENSURE WHEN RUNNING THROUGH LOOP THAT 
+               PLAYER_NAME,#COLUMNS ARE IN PROPER ORDER
+               PRIMARY_POSITION_CODE,
+               TIME_ON_ICE,
+               ASSISTS,
+               GOALS,
+               SHOTS,
+               HITS,
+               POWER_PLAY_GOALS,
+               POWER_PLAY_ASSISTS,
+               PENALTY_MINUTES,
+               FACE_OFF_WINS,
+               FACEOFF_TAKEN,
+               TAKEAWAYS,
+               GIVEAWAYS,
+               SHORT_HANDED_GOALS,
+               SHORT_HANDED_ASSISTS,
+               BLOCKED,
+               PLUS_MINUS,
+               EVEN_TIME_ON_ICE,
+               POWER_PLAY_TIME_ON_ICE,
+               SHORT_HANDED_TIME_ON_ICE,
+               FACE_OFF_PCT)
     
     return(player_data)
 }
--- scripts/player_data_extract_dev.R
+++ scripts/player_data_extract_dev.R
@@ -1,4 +1,4 @@
-test1 <- nhl_games_boxscore(game_id[2892]) %>%
+test1 <- nhl_games_boxscore(2019020001) %>%
     .[[1]] %>%
     extract2("teams") %>%
     extract2("home") %>%
@@ -26,9 +26,105 @@ test1 <- nhl_games_boxscore(game_id[2892]) %>%
                 str_to_upper) %>%
     rename_with(.,
                 str_remove,
-                pattern = "SKATER_STATS_")
+                pattern = "SKATER_STATS_") %>%
+    select(PLAYER_ID,         #CHANGED
+           PLAYER_NAME,
+           PRIMARY_POSITION_CODE,
+           TIME_ON_ICE,
+           ASSISTS,
+           GOALS,
+           SHOTS,
+           HITS,
+           POWER_PLAY_GOALS,
+           POWER_PLAY_ASSISTS,
+           PENALTY_MINUTES,
+           FACE_OFF_WINS,
+           FACEOFF_TAKEN,
+           TAKEAWAYS,
+           GIVEAWAYS,
+           SHORT_HANDED_GOALS,
+           SHORT_HANDED_ASSISTS,
+           BLOCKED,
+           PLUS_MINUS,
+           EVEN_TIME_ON_ICE,
+           POWER_PLAY_TIME_ON_ICE,
+           SHORT_HANDED_TIME_ON_ICE) %>%
+    add_column(!!!col[!names(df) %in% names(.)]) %>%
+    select(PLAYER_ID,         #CHANGED
+           PLAYER_NAME,
+           PRIMARY_POSITION_CODE,
+           TIME_ON_ICE,
+           ASSISTS,
+           GOALS,
+           SHOTS,
+           HITS,
+           POWER_PLAY_GOALS,
+           POWER_PLAY_ASSISTS,
+           PENALTY_MINUTES,
+           FACE_OFF_WINS,
+           FACEOFF_TAKEN,
+           TAKEAWAYS,
+           GIVEAWAYS,
+           SHORT_HANDED_GOALS,
+           SHORT_HANDED_ASSISTS,
+           BLOCKED,
+           PLUS_MINUS,
+           EVEN_TIME_ON_ICE,
+           POWER_PLAY_TIME_ON_ICE,
+           SHORT_HANDED_TIME_ON_ICE,
+           FACE_OFF_PCT)
 
-length(player_data)
 
-x <- "faceOffPct" %in% colnames(test1)
-x
+glimpse(test1)
+
+col <- c(PLAYER_ID = NA_integer_,
+         PLAYER_NAME = NA_character_,
+         PRIMARY_POSITION_CODE = NA_character_,
+         TIME_ON_ICE = NA_character_,
+         ASSISTS = NA_integer_,
+         GOALS = NA_integer_,
+         SHOTS = NA_integer_,
+         HITS = NA_integer_,
+         POWER_PLAY_GOALS = NA_integer_,
+         POWER_PLAY_ASSISTS = NA_integer_,
+         PENALTY_MINUTES = NA_integer_,
+         FACE_OFF_WINS = NA_integer_,
+         FACEOFF_TAKEN = NA_integer_,
+         TAKEAWAYS = NA_integer_,
+         GIVEAWAYS = NA_integer_,
+         SHORT_HANDED_GOALS = NA_integer_,
+         SHORT_HANDED_ASSISTS = NA_integer_,
+         BLOCKED = NA_integer_,
+         PLUS_MINUS = NA_integer_,
+         EVEN_TIME_ON_ICE = NA_character_,
+         POWER_PLAY_TIME_ON_ICE = NA_character_,
+         SHORT_HANDED_TIME_ON_ICE = NA_character_,
+         FACE_OFF_PCT = NA_real_)
+
+df <- data.frame(PLAYER_ID = integer(),
+                 PLAYER_NAME = character(),
+                 PRIMARY_POSITION_CODE = character(),
+                 TIME_ON_ICE = character(),
+                 ASSISTS = integer(),
+                 GOALS = integer(),
+                 SHOTS = integer(),
+                 HITS = integer(),
+                 POWER_PLAY_GOALS = integer(),
+                 POWER_PLAY_ASSISTS = integer(),
+                 PENALTY_MINUTES = integer(),
+                 FACE_OFF_WINS = integer(),
+                 FACEOFF_TAKEN = integer(),
+                 TAKEAWAYS = integer(),
+                 GIVEAWAYS = integer(),
+                 SHORT_HANDED_GOALS = integer(),
+                 SHORT_HANDED_ASSISTS = integer(),
+                 BLOCKED = integer(),
+                 PLUS_MINUS = integer(),
+                 EVEN_TIME_ON_ICE = character(),
+                 POWER_PLAY_TIME_ON_ICE = character(),
+                 SHORT_HANDED_TIME_ON_ICE = character(),
+                 FACE_OFF_PCT = double(),
+                 stringsAsFactors=FALSE)
+
+df[names(test1) == names(df)]
+
