234d493f976b0fba8c3a76e3a88a4200caed368c
jeremybutt09 <jbutt09@gmail.com>
2022-10-10 15:22:54 -0600

    adding a python script that can be called from R to extract data from database. Is more easy than using ROracle

---
+++ scripts/python/oracle_select.py
@@ -0,0 +1,15 @@
+import cx_Oracle as ora
+import pandas as pd
+
+def oracle_select(connection_string, sql_qry):
+    """"THIS FUNCTION IS USED TO MAKE A CALL TO AN ORACLE DATABASE"""
+    conStr = connection_string
+    conn = cx_Oracle.connect(conStr)
+    cur = conn.cursor()
+    sqlTxt = sql_qry
+    cur.execute(sqlTxt)
+    records = cur.fetchall()
+    df = pd.DataFrame.from_records(records, columns=[x[0] for x in cur.description])
+    cur.close()
+    conn.close()
+    return df
